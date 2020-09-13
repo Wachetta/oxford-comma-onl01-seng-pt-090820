d180b749e31324ea19be2c23087f1ff91708c326
Zachary Darneille <zakdarn@gmail.com>
2020-09-13 20:51:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ if fruit.size == 2
 fruit.join(" and ")
 elsif fruit.size == 3
   array.join(", ")
-array.insert(-2,"and")
+  array == -2 "and"
 elsif fruit.size > 3
   array.join(", ")
   array == -2 "and"
