9325da7704e55902657d681538ecacafa336a34b
Zachary Darneille <zakdarn@gmail.com>
2020-09-13 19:38:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,8 @@ if fruit.size == 2
 fruit.join(" and ")
 elsif fruit.size == 3
   array.each{|x| fruit << ", "}
-end
+  array.reject(-2)
   array.insert(-2,"and")
-end
 elsif fruit.size > 3
   array.each{|x| fruit << ", "}
   array.insert(-2,"and")
