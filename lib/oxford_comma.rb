d0ead305e48f5105078f6426725301e6b3ecf83f
Zachary Darneille <zakdarn@gmail.com>
2020-09-13 20:54:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ array.each{|x| fruit << "#{x}"}
 if fruit.size == 2
 fruit.join(" and ")
 elsif fruit.size == 3
-  array.join(", ")
   array [-2] = "and"
+  array.join(", ")
 elsif fruit.size > 3
   array.join(", ")
   array [-2] = "and" + array[-2]
