252d531293aee8a18f3b62654f341a06ea2cf74f
Zachary Darneille <zakdarn@gmail.com>
2020-09-13 19:20:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
 fruit = []
 array.each{|x| fruit << "#{x}"}
+if fruit.size == 2
 fruit.join(" and ")
-if fruit.size == 3
+elsif fruit.size == 3
   array.each{|x| fruit << ", "}
 else
   fruit[-1]
