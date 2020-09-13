fabb54865deb17597393153d8ad0c07c0ef4b907
Zachary Darneille <zakdarn@gmail.com>
2020-09-13 20:07:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 fruit = []
 array.each{|x| fruit << "#{x}"}
 if fruit.size == 2
-fruit.join(" and ")
+fruit.join("and")
 elsif fruit.size == 3
   array.insert(-2,"and")
   array.join(", ")
